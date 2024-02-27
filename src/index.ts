ba8113d7c3dc86d6351fd161f10748f5d03c0c7d
Sebastian <sebastianbarroso80@gmail.com>
2024-02-27 18:28:02 -0300

    feat: added default export for builder pattern

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-import { Calculator, CalculatorOption } from './calculator'
+import { Calculator, CalculatorOptions } from './calculator'
 
+/**
+ * Return a new Calculator instance.
+ */
+export default (options?: Partial<CalculatorOptions>) => new Calculator(options);
 
-export {
-  Calculator, 
-  CalculatorOption
-}
+export { Calculator, CalculatorOptions }
